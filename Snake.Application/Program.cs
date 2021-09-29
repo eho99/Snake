5e8c9ab46744947489b64eea044efca45c95e3c0
eho99 <ca.ericho@gmail.com>
2021-09-28 22:04:50 -0700

    Add project files.

---
+++ Snake.Application/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Snake.Application
+{
+    // ctrl+. fix problem, refactor
+    // ctrl+k,ctrl+d format
+    // ctrl+k,ctrl+c comment
+    // ctrl+k,ctrl+u uncomment
+    // ctrl+r,ctrl+r rename
+    internal static class Program
+    {
+        private static void Main()
+        {
+            
+        }
+    }
+}
