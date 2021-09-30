9e160afa57bbd7fc66f6047fd85cfcbc0dc9738c
ishanpranav <ishan.pranav@outlook.com>
2021-09-30 14:35:04 -0700

    Clean up project

--- Snake/DiagonalCaptureMoveProvider.cs
+++ Snake/DiagonalCaptureMoveProvider.cs
@@ -2,7 +2,6 @@
 // Copyright (c) 2021 Project Snake Contributors,
 // Ishan Pranav, Eric Ho, and Kaylee Kim. All rights reserved.
 
-using System;
 using System.Collections.Generic;
 
 namespace Snake
@@ -13,9 +12,7 @@ namespace Snake
         {
             Square source = piece.Square;
 
-            //need to check if there is a piece on the squares the pawn can capture on
-          
-                yield return new Move(piece, new Square(source.X + 1, source.Y + (int)piece.Player.Direction));
-                yield return new Move(piece, new Square(source.X - 1, source.Y + (int)piece.Player.Direction));
+            yield return new Move(piece, new Square(source.X + 1, source.Y + (int)piece.Player.Direction));
+            yield return new Move(piece, new Square(source.X - 1, source.Y + (int)piece.Player.Direction));
     }
 }
